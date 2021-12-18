4412334175467ccdd4ccbf13f310e144f0ab0344
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-18 13:04:12 -0800

    Added form and search function structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from "./logo-YR.PNG";
 import "./App.css";
+import Dictionary from "./Dictionary";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            {" "}
+            This project was created by Yarelys Rivera, hosted on Netlify and is{" "}
+            <a href="https://github.com/YarelysRR/dictionary-project">
+              open-sourced
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
