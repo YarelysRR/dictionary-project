1999abbdbc28bc65ead5e4bc43817726f67adad5
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-18 10:50:39 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo-YR.PNG";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
