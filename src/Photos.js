818b19694e16fd2a2e106dfd723b3ddb38c012bd
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-19 21:28:30 -0800

    Updated to empty alt

--- src/Photos.js
+++ src/Photos.js
@@ -9,11 +9,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.landscape}
-                    className="img-fluid"
-                    alt="Photo from search"
-                  />
+                  <img src={photo.src.landscape} className="img-fluid" alt="" />
                 </a>
               </div>
             );
