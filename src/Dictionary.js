2b2bc01cbe37495361388e69aac50027f442c7b3
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-19 21:17:37 -0800

    Added photos and improve styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Photos from "./Photos";
 import Results from "./Results";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState("");
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000013b540d4fb04f4a8ba2c95877c9aae080";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -50,6 +63,7 @@ export default function Dictionary(props) {
           </section>
 
           <Results results={results} />
+          <Photos photos={photos} />
         </div>
       </div>
     );
